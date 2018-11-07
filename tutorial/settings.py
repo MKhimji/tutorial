c9c5e09099306a510a8d834bbc241bff9ae639a6
MKhimji <muhammadkhimji@live.co.uk>
2018-11-07 02:43:20 +0000

    updated settings.py

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -103,22 +103,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 
-#Database
-#https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#          'NAME': 'tutorial',
-#          'USER': 'm',
-#          : '',
-#          'HOST': 'localhost',
-#          'PORT': '5432',
-
- 
-
-#      }
-#  }
 
 DATABASES = {
     'default': env.db(), # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
